67e90e6eb1a9b2a333960f0ed1f54c4ccd16a8a4
OakAlice <oakleigh.wilson05@gmail.com>
2024-01-09 21:11:58 +1000

    now we can pick bounding boxes specifically in the dataframe coordinates

--- jack.py
+++ jack.py
@@ -1,6 +1,8 @@
 from PyQt6.QtWidgets import QApplication
 import sys
-    
+from dataclasses import dataclass
+from typing import List, Tuple
+
 from matplotlib.backend_bases import MouseEvent
 from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
@@ -33,35 +35,50 @@ class FriendlyFigureCanvas(FigureCanvas):
     def __init__(self, figure: Figure):
         super().__init__(figure=figure)
     
-    def mousePressEvent(self, event):
+    def mousePressEvent(self, event: MouseEvent):
         super().mousePressEvent(event)
         self.mousePressEventSignal.emit(event)
 
-    def mouseMoveEvent(self, event):
+    def mouseMoveEvent(self, event: MouseEvent):
         super().mouseMoveEvent(event)
         self.mouseMoveEventSignal.emit(event)
 
-    def mouseReleaseEvent(self, event):
+    def mouseReleaseEvent(self, event: MouseEvent):
         super().mouseReleaseEvent(event)
         self.mouseReleaseEventSignal.emit(event)
 
+@dataclass
+class Behaviour:
+    title: str
+    colour: str
 
 class PlotWidget(QWidget):
     def __init__(self, parent=None, file_path: str = "Maple1_on_GOPR0434.csv"):
         super().__init__(parent)
         self.setWindowTitle("Jack was here")
 
-
         self.figure, self.ax = plt.subplots()
         self.canvas = FriendlyFigureCanvas(self.figure)
+ 
+        # Connect up mouse events on the canvas to our class.
+        self.canvas.mouseMoveEventSignal.connect(self.canvasMouseMoveEvent)
+        self.canvas.mousePressEventSignal.connect(self.canvasMousePressEvent)
+        self.canvas.mouseReleaseEventSignal.connect(self.canvasMouseReleaseEvent)
+
+        # Add dataframe to figure
         df = read_and_process_csv(file_path)
         self.dataframe = df
+        self.hardcoded_dataframe_samples = 1000 # How many frames we will consider at once. Currently cant scroll.
         self.plot_data(0)
 
         # Create the layout so that we can add things to the Plotwidget
         layout = QVBoxLayout()
         layout.addWidget(self.canvas)
         self.setLayout(layout)
+
+        self.current_idx = -1
+        self.behaviours: List[Behaviour] = []
+        self.current_bounding_box: List[int, int] = [-1, -1]
 
     def plot_data(self, start_index):
         print(f"self.dataframe is not None: {self.dataframe}, {self.dataframe is not None}")
@@ -72,10 +89,37 @@ class PlotWidget(QWidget):
             self.ax.plot(self.dataframe.iloc[:, 0], self.dataframe.iloc[:, 3], color='blue', linewidth=0.5)
             self.figure.subplots_adjust(left=0, right=1, top=1, bottom=0)
 
-            end_index = min(len(self.dataframe), 1000)
+            end_index = min(len(self.dataframe), self.hardcoded_dataframe_samples)
             self.ax.set_xlim(self.dataframe.iloc[0, 0], self.dataframe.iloc[end_index - 1, 0])
 
             self.canvas.draw()
+
+    def canvasMousePressEvent(self, event: MouseEvent):
+        if self.dataframe is not None:
+            self.current_bounding_box[0] = self.get_dataframe_index_of_mouse(event)
+
+    def canvasMouseMoveEvent(self, event: MouseEvent):
+        pass
+        # if self.dataframe is not None:
+        #     print(f"mouse movee event, {self.get_relative_mouse_position(event)}")
+            
+    def canvasMouseReleaseEvent(self, event: MouseEvent):
+        if self.dataframe is not None:
+            self.current_bounding_box[1] = self.get_dataframe_index_of_mouse(event)
+            print(f"Add behaviour between frames: {self.current_bounding_box}")
+            self.current_bounding_box = [-1, -1]
+
+    def get_relative_mouse_position(self, event) -> Tuple[float, float]:
+        """
+        This calculates the relative x, y co-ordinates of the mouse event relative to the canvas. 
+        Coordinates are normalised between [0, 1.0] with the top left corner being (0, 0).
+        """
+        return event.pos().x()/self.canvas.width(), event.pos().y()/self.canvas.height()
+
+    def get_dataframe_index_of_mouse(self, event: MouseEvent) -> float:
+        """Compute where, x-coordinate, in the self.dataframe the mouse is currently at. This works for all mouse events."""
+        # TODO: once we support scrolling + zoom, we will need to worry about offsets/conversions etc.
+        return self.get_relative_mouse_position(event)[0] * self.hardcoded_dataframe_samples
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
